fcf080f59541f6fbaf1f97424a05e61bd776e528
GuillaumeTaffin <guillaume.taffinjob@gmail.com>
2025-08-10 13:46:17 -0400

    Init project with Gradle

---
+++ lib/build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    `java-library`
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(libs.junit.jupiter)
+
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
